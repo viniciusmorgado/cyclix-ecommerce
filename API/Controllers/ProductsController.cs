13b3ebd9c262b48087815de06fa1481503bc8fd4
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-11-04 19:28:28 -0300

    Initial implementation of ProductController

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "A list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "Single Product";
+    }
+    }
+}
