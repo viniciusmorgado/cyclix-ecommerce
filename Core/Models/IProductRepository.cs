b65d69e4c4e585cdab1bafb6dffdb04d05187513
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-11-05 22:05:50 -0300

    Implements a Product repository interface for Repository Pattern
    - Implementation of IProductRepository for repository pattern inside
      Core project.
    - Implementation of ProductRepository inside Core project;

--- Core/Models/IProductRepository.cs
+++ Core/Models/IProductRepository.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace Core.Models
 {
     public interface IProductRepository
     {
-        
+        Task<Product> GetProductByIdAsync(int Id);
+        Task<IReadOnlyList<Product>> GetProductListAsync();
     }
 }
