b1127121a1fb3750ee340a3900172b9e8c89fe02
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-11-14 16:09:17 -0300

    Implemente repository pattern in ProductRepository

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -1,7 +1,20 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Models;
+using Core.Models.Repositories;
+
 namespace Infrastructure.Data
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository
     {
-        
+        public Task<Product> GetProductByIdAsync(int Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Product>> GetProductListAsync()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
