48e4c97c215ac7ee08dd7d13c3c8148973f456a0
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-11-14 20:27:41 -0300

    Implements product controllers methods

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,6 +3,8 @@ using Infrastructure.Data;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using Core.Models.IRepositories;
+using Infrastructure.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
@@ -11,23 +13,22 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repo;
 
-        public ProductsController(StoreContext context)
-            => _context = context;
+        public ProductsController(IProductRepository repo)
+            => _repo = repo;
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _repo.GetProductListAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await _context.Products.FindAsync(id);
-            return Ok(product);
+            return await _repo.GetProductByIdAsync(id);
         }
     }
 }
