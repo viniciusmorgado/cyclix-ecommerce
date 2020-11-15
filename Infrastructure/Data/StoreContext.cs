e1f4bfc37e274bdf09f83aa2d54f910a2f5e6f65
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-11-14 23:26:14 -0300

    Add ProductBrand and ProductType class, implement new maigrations and remove the old ones

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -8,5 +8,7 @@ namespace Infrastructure.Data
         public StoreContext(DbContextOptions<StoreContext> options) : base (options) {}
     
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductType { get; set; }
+        public DbSet<ProductBrand> ProductBrand { get; set; }
     }
 }
