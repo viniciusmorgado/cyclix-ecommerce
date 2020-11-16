5f742887e1f3f63f82f8ba7c5266ed9d64ad7cee
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-11-16 20:08:56 -0300

    Implements Config directory for add configuration class of FluentAPI and start the implementation of ProductConfiguration class

---
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(prop => prop.Id).IsRequired();
+            builder.Property(prop => prop.Name).HasMaxLength(100);
+        }
+    }
+}
