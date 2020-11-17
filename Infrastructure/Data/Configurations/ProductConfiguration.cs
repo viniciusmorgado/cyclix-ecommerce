6545b2817e2a0a1d36ff9e7ceec5b714d0fece50
viniciusmorgado <contato.viniciusdonatto@gmail.com>
2020-11-16 21:00:39 -0300

    Complete the implemantation of Product class configuration

--- Infrastructure/Data/Configurations/ProductConfiguration.cs
+++ Infrastructure/Data/Configurations/ProductConfiguration.cs
@@ -13,6 +13,13 @@ namespace Infrastructure.Data.Config
         {
             builder.Property(prop => prop.Id).IsRequired();
             builder.Property(prop => prop.Name).IsRequired().HasMaxLength(100);
+            builder.Property(prop => prop.Description).IsRequired().HasMaxLength(180);
+            builder.Property(prop => prop.Price).HasColumnType("decimal(18,2)");
+            builder.Property(prop => prop.PictureUrl).IsRequired();
+            builder.HasOne(prop => prop.ProductBrand).WithMany()
+                .HasForeignKey(key => key.ProductBrandId);
+            builder.HasOne(prop => prop.ProductType).WithMany()
+                .HasForeignKey(key => key.ProductTypeId);
         }
     }
 }
